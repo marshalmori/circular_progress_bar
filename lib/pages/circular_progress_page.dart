47bf4b608cea6c89304d21f52d5299420d06cc0b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2021-06-09 22:28:44 -0300

    Create circle and Arc

--- lib/pages/circular_progress_page.dart
+++ lib/pages/circular_progress_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class CircularProgressPage extends StatefulWidget {
@@ -11,11 +13,51 @@ class _CircularProgressPageState extends State<CircularProgressPage> {
     return Scaffold(
       body: Center(
         child: Container(
+          padding: EdgeInsets.all(5),
           width: 300,
           height: 300,
-          color: Colors.red,
+          //color: Colors.red,
+          child: CustomPaint(
+            painter: _RadialProgress(40),
+          ),
         ),
       ),
     );
   }
+}
+
+class _RadialProgress extends CustomPainter {
+  final percentage;
+
+  _RadialProgress(this.percentage);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint()
+      ..strokeWidth = 4
+      ..color = Colors.blueGrey
+      ..style = PaintingStyle.stroke;
+
+    Offset center = new Offset(size.width * 0.5, size.height / 2);
+    double radius = min(size.width * 0.5, size.height * 0.5);
+
+    canvas.drawCircle(center, radius, paint);
+
+    final paintArc = new Paint()
+      ..strokeWidth = 10
+      ..color = Colors.orange.shade800
+      ..style = PaintingStyle.stroke;
+
+    double arcAngle = 2 * pi * (percentage / 100);
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      -pi / 2,
+      arcAngle,
+      false,
+      paintArc,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
